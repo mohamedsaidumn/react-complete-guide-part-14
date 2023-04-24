432d1b4b7176a09359d5c3b5ef95967dccd1a4c9
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-04-24 16:50:58 -0500

    adding links

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <ProductsPage /> },
-// ]);
-
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
-const router = createBrowserRouter(routeDefinitions);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
