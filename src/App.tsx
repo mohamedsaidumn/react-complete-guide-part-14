fc924608be209856f6cc4edaea4e04152225e733
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-04-24 17:31:17 -0500

    add a layout wrapper route

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 
 function App() {
