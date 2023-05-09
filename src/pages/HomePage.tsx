4c7af85b65e289ab30da0182481a885a49ff8841
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-05-09 06:37:45 -0500

    use the hook useNavigate to programatically navigate to a page.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function HomePage() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      navigate("/products");
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, [navigate]);
   return (
     <>
       <h1>My Home Page</h1>
