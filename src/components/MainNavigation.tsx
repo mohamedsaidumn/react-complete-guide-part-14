ffd20ae99895f3722d55c1c73519aba2709dbe8b
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-05-09 10:56:44 -0500

    turned my childres routes and links, navlinks to relatives paths instead of absolute paths

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -9,7 +9,7 @@ function MainNavigation() {
         <ul className={classes.list}>
           <li>
             <NavLink
-              to="/"
+              to=""
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
@@ -23,7 +23,7 @@ function MainNavigation() {
           </li>
           <li>
             <NavLink
-              to="/products"
+              to="products"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,6 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 function HomePage() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      navigate("/products");
-    }, 1000);
-
-    return () => clearTimeout(timeoutId);
-  }, [navigate]);
   return (
     <>
       <h1>My Home Page reroutes in 30 second</h1>
